9c7e5bc2adf2ef1b7831d9d49c8b44a7463970a9
Ggwepq <ggwepq@gmail.com>
2024-04-07 17:19:22 +0800

    Display products from db to product list ui

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class ProductController extends Controller
 {
     public function index(){
-        return Inertia::render('Admin/Product/Index');
+        $products = Product::get();
+        return Inertia::render('Admin/Product/Index', ['products' => $products]);
     }
 }
