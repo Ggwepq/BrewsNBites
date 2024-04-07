fb465a1b04b43ef96fe8f9673deaa74125b5244c
Ggwepq <ggwepq@gmail.com>
2024-04-07 17:03:32 +0800

    Fix product table not working properly

--- resources/js/Pages/Admin/Components/AdminLayout.vue
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -9,8 +9,9 @@
             <Sidebar/>
         <!-- Sidebar End -->
 
-        <slot/>
-
+        <main class="p-4 md:ml-64 h-auto pt-20">
+            <slot/>
+        </main>
     </div>
 </template>
 
--- resources/js/Pages/Admin/Product/Index.vue
+++ resources/js/Pages/Admin/Product/Index.vue
@@ -1,7 +1,12 @@
 <template>
-    <AdminLayout/>
+    <AdminLayout>
+        <ProductList>
+
+        </ProductList>
+    </AdminLayout>
 </template>
 
 <script setup>
     import AdminLayout from '../Components/AdminLayout.vue';
+    import ProductList from './ProductList.vue';
 </script>
