376a3c5c306f9169741a68dd955a10e7ee5fd9c3
Ggwepq <ggwepq@gmail.com>
2024-04-07 15:06:04 +0800

    Auto generate slug from title for db

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,7 +5,30 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
 class Product extends Model
 {
+    use HasSlug;
     use HasFactory;
+    
+    protected $fillable = [
+       'title',
+       'slug',
+       'description',
+       'price',
+       'published',
+       'in_stock',
+       'created_by',
+       'deleted_by',
+       'updated_by'
+    ];
+
+    public function getSlugOptions() : SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
 }
