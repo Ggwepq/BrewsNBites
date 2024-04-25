7687eb59121d2ac7482ad91d286c0bda1ba3151e
Niggarl The White Man <brickpolsly@gmail.com>
2024-04-25 11:42:28 +0000

    Fix log out not working

--- resources/js/Pages/User/Layout/Header.vue
+++ resources/js/Pages/User/Layout/Header.vue
@@ -75,9 +75,9 @@ const cart = computed(()=>usePage().props.cart);
                                 class="transition-all block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
                         </li>
                         <li>
-                            <a href="#"
+                            <Link :href="route('logout')" method="post"
                                 class="transition-all block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
-                                out</a>
+                                out</Link>
                         </li>
                     </ul>
                 </div>
